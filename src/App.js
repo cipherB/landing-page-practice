cc657191914b5557b14e5d81a54812873b1d730b
cypherB <bolulawal5@gmail.com>
2022-07-28 15:18:16 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="w-20 h-20 font-bold text-white bg-green-500 cursor-pointer lg:bg-blue-800 hover:bg-red-600" 
+      >
+        Hello THere
+      </h1>
     </div>
   );
 }
